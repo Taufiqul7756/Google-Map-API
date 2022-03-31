62c578176785c0cf538ddefd843323086c699ee8
Taufiqul Islam <48012661+Taufiqul7756@users.noreply.github.com>
2022-03-31 21:13:49 +0600

    Origin and Destination Path added Dynamically

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-
-import Map from './Components/Map';
-import Direction from './Components/Direction';
+import Map from "./Components/Map";
+import Direction from "./Components/Direction";
+import { useState } from "react";
 
 function App() {
+  const [origin, setOrigin] = useState("");
+  const [destination, setDestination] = useState("");
+
   return (
     <div className="App">
-     <Direction></Direction>
+      <input
+        type="text"
+        onBlur={(e) => setOrigin(e.target.value)}
+        placeholder="Starting From"
+      />
+      <input
+        type="text"
+        onBlur={(e) => setDestination(e.target.value)}
+        placeholder="Going to "
+      />
+      <Direction origin={origin} destination={destination}></Direction>
     </div>
   );
 }
--- src/Components/Direction.js
+++ src/Components/Direction.js
@@ -1,5 +1,10 @@
 import React, { useState } from "react";
-import { GoogleMap, LoadScript,DirectionsService,DirectionsRenderer } from "@react-google-maps/api";
+import {
+  GoogleMap,
+  LoadScript,
+  DirectionsService,
+  DirectionsRenderer,
+} from "@react-google-maps/api";
 
 const containerStyle = {
   width: "400px",
@@ -11,40 +16,36 @@ const Location = {
   lng: 90.377713,
 };
 
-function Direction() {
-    const [directionResponse , setDirectionResponse] = useState(null);
+function Direction({ origin, destination }) {
+  const [directionResponse, setDirectionResponse] = useState(null);
   return (
     <LoadScript googleMapsApiKey="YOUR_API_KEY">
       <GoogleMap mapContainerStyle={containerStyle} center={Location} zoom={10}>
+        {origin !== "" && destination !== "" && (
+          <DirectionsService
+            options={{
+              destination: {destination},
+              origin: {origin},
+              travelMode: "DRIVING",
+            }}
+            callback={(res) => {
+              if (res !== null) {
+                setDirectionResponse(res);
+              }
+            }}
+          />
+        )}
 
-      <DirectionsService
-                  options={{ 
-                    destination: 'United Hospital Dhaka Bangladesh',
-                    origin: 'Banani 11 City Bank Dhaka Bangladesh',
-                    travelMode: 'DRIVING'
-                  }}
-                  
-                  callback={res=>{
-                      if(res !== null){
-                        setDirectionResponse(res);
-                      }
-                  }}
-                />
-
-                {
-                    directionResponse && <DirectionsRenderer
-                    options={{
-                      directions: directionResponse
-                    }}
-                    
-                  />
-                }
-
+        {directionResponse && (
+          <DirectionsRenderer
+            options={{
+              directions: directionResponse,
+            }}
+          />
+        )}
       </GoogleMap>
     </LoadScript>
   );
 }
 
 export default React.memo(Direction);
-
-
